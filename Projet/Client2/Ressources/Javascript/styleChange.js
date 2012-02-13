9a77e0996fab7bbf9b501c1cfdc2bb242f127597
bapismej@gmail.com <bapismej@gmail.com@04c724b0-ad50-78d0-5040-20067cc3e712>
2012-02-13 12:14:33 +0000

    slider debug + search input/div debug

--- Projet/Client2/Ressources/Javascript/styleChange.js
+++ Projet/Client2/Ressources/Javascript/styleChange.js
@@ -1,19 +1,47 @@
 function init(){
-	$("#searchInput").width($("#searchDiv").width() - 50);
+	resized();
+	var min = 1;
+	var max = 20;
+	$("#slider").slider({
+		max : max,
+		min : min,
+		value : (max-min)/2,
+		step : 1,
+		orientation : 'vertical',
+		slide : function(event, ui){
+			zoomChange(ui.value);
+		}
+	});
+	$("#topLabelSlider").html(max);
+	$("#bottomLabelSlider").html(min);
+}
+
+function resized(){
+	$("#searchInput").width($("#searchDiv").width() - 70);
 }
 
 function ok(){
-	//moveTo("searchDiv",15,5,20);
-	$('.index').animate({
-		opacity: 0
-	},500, function(){
-		$('#searchDiv').animate({
-			top: '-=280'
-		},500,function() {
-				$('#main').animate({
-					opacity : 100
-				},5000,function() {
+	if($('#searchDiv').position().top > 20){
+		$('.index').animate({
+			opacity: 0
+		},500, function(){
+			$('#searchDiv').animate({
+				top: '-=280'
+			},500,function() {
+					$('#zoomSlider').animate({
+						opacity : 100
+					},500,function() {
+						$('#main').animate({
+							opacity : 100
+						},5000,function() {
+					});
+				});
 			});
 		});
-	});
+	}
+}
+
+function clickInputSearch(){
+	var currentVal = $('#searchInput').val();
+	if( currentVal == "Veuillez saisir votre recherche" ){ $("#searchInput").val(""); }
 }
