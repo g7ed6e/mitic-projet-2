5f9101cbbe9ebe0bb14f76d64da84ef17c85fa7f
beben35170@gmail.com <beben35170@gmail.com@04c724b0-ad50-78d0-5040-20067cc3e712>
2012-02-14 10:03:08 +0000

    Gestions de différents formats d'images

--- trunk/Projet/Server/View/ImageView.php
+++ trunk/Projet/Server/View/ImageView.php
@@ -1,13 +1,29 @@
 <?php
 class ImageView implements View{
 	private $data;
-	
+	private $format;
+
 	public function __construct($data){
-		$this->data = $data;
+		$this->data = $data[1];
+		$this->format = $data[0];
 	}
-	
+
 	public function Render(){
-		header("Content-Type: image/jpeg");
-		imagejpeg($this->data);
+
+		if ($this->format=='image/jpeg' ) {
+			header("Content-Type: image/jpeg");
+			imagejpeg($this->data);
+		}
+		elseif ($this->format=='image/png' ) {
+			header("Content-Type: image/png");
+			imagepng($this->data);
+		}
+		elseif ($this->format=='image/gif' ) {
+			header("Content-Type: image/gif");
+			imagegif($this->data);
+		}
+
+
 	}
+
 }
