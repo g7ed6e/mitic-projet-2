9f3c3f806a69323c04ed001df908948d4a9ce9b5
wujixx@gmail.com <wujixx@gmail.com@04c724b0-ad50-78d0-5040-20067cc3e712>
2012-02-16 11:09:48 +0000

    svg arrive !

---
+++ Projet/Client2/Ressources/Javascript/GraphSVG.js
@@ -0,0 +1,183 @@
+var graph;
+var canvas;
+var ctx;
+var graphCenter;
+$(document).ready(function(){
+	canvas = $("#svg").get(0);
+	ctx = canvas.getContext('2d'); 
+	ctx.canvas.width  = $("#main").innerWidth();
+	ctx.canvas.height = $("#main").innerHeight();
+	$("#canvas").width($("#main").innerWidth());
+	graphCenter = {"x": $("#main").width() / 2, "y": $("#main").height() / 2};
+
+	graph = new Graph();
+
+	//request(1, 24);
+	
+});
+
+function Graph(options) {
+	  this.options = options || {};
+	  this.nodeSet = {};
+	  this.nodes = [];
+	  this.edges = [];
+	  this.layout;
+	}
+
+
+	Graph.prototype.addNode = function(node) {
+	  if(this.nodeSet[node.id] == undefined && !this.reached_limit()) {
+	    this.nodeSet[node.id] = node;
+	    this.nodes.push(node);
+	    return true;
+	  }
+	  return false;
+	};
+
+	Graph.prototype.getNode = function(node_id) {
+	  return this.nodeSet[node_id];
+	};
+	
+	Graph.prototype.getNodes = function(){
+		return this.nodeSet;
+	}
+
+	Graph.prototype.clearNodes = function() {
+		this.nodeSet = {};
+		this.nodes = [];
+		this.edges = [];
+	};	
+	
+	Graph.prototype.addEdge = function(source, target) {
+	  if(source.addConnectedTo(target) === true) {
+	    var edge = new Edge(source, target);
+	    this.edges.push(edge);
+	    return true;
+	  }
+	  return false;
+	};
+
+	Graph.prototype.reached_limit = function() {
+	  if(this.options.limit != undefined)
+	    return this.options.limit <= this.nodes.length;
+	  else
+	    return false;
+	};
+
+
+	function Node(node_id, x, y, width, height) {
+	  this.id = node_id;
+	  this.width = width || 0;
+	  this.height = height || 0;
+	  this.center = {"x" : x + graphCenter.x, "y" : y + graphCenter.y};
+	  this.position = {"x" : this.center.x - (this.width/2), "y" : this.center.y - (this.height/2)};
+	  this.position2 = {"x" : this.center.x + (this.width/2), "y" : this.center.y + (this.height/2)};
+	  this.nodesTo = [];
+	  this.nodesFrom = [];
+	  this.data = {};
+	}
+	
+	Node.prototype.addConnectedTo = function(node) {
+	  if(this.connectedTo(node) === false) {
+	    this.nodesTo.push(node);
+	    return true;
+	  }
+	  return false;
+	};
+
+	Node.prototype.connectedTo = function(node) {
+	  for(var i=0; i < this.nodesTo.length; i++) {
+	    var connectedNode = this.nodesTo[i];
+	    if(connectedNode.id == node.id) {
+	      return true;
+	    }
+	  }
+	  return false;
+	};
+
+
+	function Edge(source, target) {
+	  this.source = source;
+	  this.target = target;
+	  this.data = {};
+	}
+		
+	function drawNode(){
+		var img=new Image();
+	        img.src="../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50";
+	        // on attend le chargement complet de l'image pour l'insérer dans le canvas
+	        jQuery(img).load(function() {
+		        node.width = img.width;
+		        node.height = img.height;
+		        node.position = {"x" : node.center.x - (node.width/2), "y" : node.center.y - (node.height/2)};
+		  	  	node.position2 = {"x" : node.center.x + (node.width/2), "y" : node.center.y + (node.height/2)};
+		  	    ctx.drawImage(img,node.position.x, node.position.y, img.width, img.height);
+	      });
+		 
+		var x1 = 
+		$("#svg").append($("<image x="+node.center.x - (node.width/2)+
+				" y="+node.center.y - (node.height/2)+
+				" width="node.width+
+				" height="+node.height+
+				" xlink:href=\"../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50\""+
+				" />"));
+	}
+	
+	function drawEdge(edge){
+		$("#svg").prepend($("<line x1="+edge.source.center.x+
+				"y1="+edge.source.center.y+
+				" x2="+edge.target.center.x+
+				" y2="+edge.target.center.y+
+				"style=\"stroke:rgb(0,0,0);stroke-width:0.5\" />"););
+		//<line x1="0" y1="0" x2="200" y2="200"
+		//style="stroke:rgb(255,0,0);stroke-width:2"/>
+	}
+
+	function request(id, nbNeighbours){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.width = canvas.width;
+
+		$.ajax({
+			  url: '../Server/index.php',
+			  dataType: 'json',
+			  cache: false,
+			  data: {"controller" : "voisinsN", "id" : id, "nn" : nbNeighbours, "action" : "getVoisinsN", "w": canvas.width, "h": canvas.height},
+			  success: function(data) {
+					graph.clearNodes();
+					var liens = data.liens;
+					var positions = data.positions;
+					for(var i in positions){
+						var node = new Node(positions[i][0], positions[i][1], positions[i][2]);
+						graph.addNode(node);
+					}	
+					for(var i in liens){				
+						graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
+						drawEdge(graph.edges[i]);				
+					}
+					for(var i=0; i<graph.nodes.length; i++)
+						drawNode(graph.nodes[i]);
+
+
+			  }
+		});
+			 
+		
+		/*
+		$.getJSON('../Server/index.php?controller=voisinsN&action=getVoisinsN&id='+id+'&nn='+nbNeighbours, function(data) {
+			graph.clearNodes();
+			var liens = data.liens;
+			var positions = data.positions;
+			for(var i in positions){
+				var node = new Node(positions[i][0], positions[i][1] * ratio, positions[i][2] * ratio);
+				graph.addNode(node);
+			}	
+			for(var i in liens){				
+				graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
+				drawEdge(graph.edges[i]);				
+			}
+			for(var i=0; i<graph.nodes.length; i++)
+				drawNode(graph.nodes[i]);
+
+
+		});*/
+	}
