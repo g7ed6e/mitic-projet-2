edceb50e26de9a946c6e75258b1242250508e374
wujixx@gmail.com <wujixx@gmail.com@04c724b0-ad50-78d0-5040-20067cc3e712>
2012-02-19 22:27:29 +0000

    Graph SVG à partir des nouveaux document l'arbre SVG est bien généré mais n'apparaît pas dans la page ( un c/c sur une page vierge le fait en revanche apparaître )

--- trunk/Projet/Client2/Ressources/Javascript/GraphSVG.js
+++ trunk/Projet/Client2/Ressources/Javascript/GraphSVG.js
@@ -1,19 +1,42 @@
 var graph;
 var canvas;
 var ctx;
+var mignatureSize = 50;
 var graphCenter;
+var algoNPlusUn = false;
+var renduSVG = false;
+var zoom = 1;
+var HTML = true;
+var dataMatrice = false;
+
 $(document).ready(function(){
-	canvas = $("#svg").get(0);
+
+	
+	canvas = $("#canvas").get(0);
 	ctx = canvas.getContext('2d'); 
-	ctx.canvas.width  = $("#main").innerWidth();
-	ctx.canvas.height = $("#main").innerHeight();
-	$("#canvas").width($("#main").innerWidth());
-	graphCenter = {"x": $("#main").width() / 2, "y": $("#main").height() / 2};
+	ctx.canvas.width  = $("#main").innerWidth() - $("#zoomDiv").innerWidth();
+	ctx.canvas.height = $("#main").innerHeight()-$("#canvas").get(0).offsetTop-$("#footer").innerHeight()-10;
+	$("#svgDiv").width(ctx.canvas.width);
+	$("#svgDiv").height(ctx.canvas.height);
+	graphCenter = {"x":ctx.canvas.width / 2, "y": ctx.canvas.height / 2};
+	$("#fg").css("left", 0).css("top", $("#canvas").get(0).offsetTop + graphCenter.y-23+"px").click(function(){
+		graphCenter.x -= 75;
+		draw();
+	});
+	$("#fd").css("left", ctx.canvas.width-35).css("top", $("#canvas").get(0).offsetTop + graphCenter.y-23 +"px").click(function(){
+		graphCenter.x += 75;
+		draw();
+	});
+	$("#fh").css("left", graphCenter.x-24).css("top", $("#canvas").get(0).offsetTop).click(function(){
+		graphCenter.y -= 75;
+		draw();
+	});
+	$("#fb").css("left", graphCenter.x-24).css("top", $("#canvas").get(0).offsetTop + ctx.canvas.height -35+"px").click(function(){
+		graphCenter.y += 75;
+		draw();
+	});
 
 	graph = new Graph();
-
-	//request(1, 24);
-	
 });
 
 function Graph(options) {
@@ -69,9 +92,9 @@ function Graph(options) {
 	  this.id = node_id;
 	  this.width = width || 0;
 	  this.height = height || 0;
-	  this.center = {"x" : x + graphCenter.x, "y" : y + graphCenter.y};
-	  this.position = {"x" : this.center.x - (this.width/2), "y" : this.center.y - (this.height/2)};
-	  this.position2 = {"x" : this.center.x + (this.width/2), "y" : this.center.y + (this.height/2)};
+	  this.center = {"x" : x, "y" : y};
+	  this.position = {"x" : this.center.x - (this.width/2)  + graphCenter.x, "y" : this.center.y - (this.height/2) + graphCenter.y};
+	  this.position2 = {"x" : this.center.x + (this.width/2)  + graphCenter.x, "y" : this.center.y + (this.height/2) + graphCenter.y};
 	  this.nodesTo = [];
 	  this.nodesFrom = [];
 	  this.data = {};
@@ -101,83 +124,122 @@ function Graph(options) {
 	  this.target = target;
 	  this.data = {};
 	}
+	function request(id, nbNeighbours){
 		
-	function drawNode(){
-		var img=new Image();
-	        img.src="../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50";
+		if(!algoNPlusUn){ 			
+			$.ajax({
+				  url: '../Server/index.php',
+				  dataType: 'json',
+				  cache: false,
+				  data:  {"controller" : "voisinsN", "id" : id, "nn" : nbNeighbours, "action" : "getVoisinsN", "w": canvas.width, "h": canvas.height},
+				  success: function(data) {
+						graph.clearNodes();
+						var liens = data.liens;
+						var positions = data.positions;
+						for(var i in positions){
+							var node = new Node(positions[i][0], positions[i][1], positions[i][2]);
+							graph.addNode(node);
+						}	
+						for(var i in liens)			
+							graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
+					
+						draw();
+
+				  }
+			});	
+		}
+		else{
+			$.ajax({
+				  url: '../Server/index.php',
+				  dataType: 'json',
+				  cache: false,
+				  data:   {"controller" : "voisinsNPlusUn", "id" : id, "nn" : nbNeighbours, "nnPlusUn" : nbNeighbours, "action" : "getVoisinsNPlusUn", "w": canvas.width, "h": canvas.height},
+				  success: function(data) {
+						graph.clearNodes();
+						var liens = data.liens;
+						var positions = data.positions;
+						for(var i in positions){
+							var node = new Node(positions[i][0], positions[i][1], positions[i][2]);
+							graph.addNode(node);
+						}	
+						for(var i in liens)			
+							graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
+					
+						draw();
+
+				  }
+			});	
+			}
+			
+		
+		
+			 
+	}
+	
+	function draw(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.width = canvas.width;
+		
+		for(var i in graph.edges)				
+			drawEdge(graph.edges[i]);				
+
+		for(var i in graph.nodes)
+			drawNode(graph.nodes[i]);
+		
+	}
+	
+		function drawNode(node){
+		/* if (canvas.getContext)
+	    {    
+	        
+	        var img=new Image();
+	        img.src="../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=" + mignatureSize;
 	        // on attend le chargement complet de l'image pour l'insérer dans le canvas
 	        jQuery(img).load(function() {
 		        node.width = img.width;
 		        node.height = img.height;
-		        node.position = {"x" : node.center.x - (node.width/2), "y" : node.center.y - (node.height/2)};
-		  	  	node.position2 = {"x" : node.center.x + (node.width/2), "y" : node.center.y + (node.height/2)};
-		  	    ctx.drawImage(img,node.position.x, node.position.y, img.width, img.height);
-	      });
-		 
-		var x1 = 
-		$("#svg").append($("<image x="+node.center.x - (node.width/2)+
-				" y="+node.center.y - (node.height/2)+
-				" width="node.width+
-				" height="+node.height+
-				" xlink:href=\"../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50\""+
-				" />"));
+		  	    console.log(node.center);
+		  	    console.log(zoom);
+
+		        node.position = {"x" : (node.center.x * zoom) - (node.width/2) + graphCenter.x, "y" : (node.center.y * zoom) - (node.height/2) + graphCenter.y};
+		  	  	node.position2 = {"x" : (node.center.x * zoom) + (node.width/2) + graphCenter.x, "y" : (node.center.y * zoom) + (node.height/2) + graphCenter.y};
+		  	  //  console.log(node.position);
+		  	  	ctx.fillRect(node.position.x, node.position.y, img.width, img.height);       // afficher un rectangle plein
+		        ctx.drawImage(img,node.position.x, node.position.y, img.width, img.height);
+	        });
+	    } */
+		
+		img=new Image();
+	    img.src="../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50";
+		        node.width = img.width;
+		        node.height = img.height;
+		  	    console.log(node.center);
+		  	    console.log(zoom);
+		var imgX = ( node.center.x - (node.width/2) );
+		var imgY = ( node.center.y - (node.height/2) );
+		var imgWidth = (node.center.x * zoom) - (node.width/2) + graphCenter.x;
+		var imgHeight = (node.center.y * zoom) - (node.height/2) + graphCenter.y;
+		alert("<image x=\""+imgX+
+				"\" y=\""+imgY+
+				"\" width=\""+imgWidth+
+				"\" height=\""+imgHeight+
+				"\" xlink:href=\"../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50\""+
+				" />");
+		$("#svgDiv").append(
+			$("<image x=\""+imgX+
+				"\" y=\""+imgY+
+				"\" width=\""+imgWidth+
+				"\" height=\""+imgHeight+
+				"\" xlink:href=\"../Server/index.php?controller=image&action=getImg&id="+node.id+"&t=50\""+
+				" />")
+		);
 	}
-	
 	function drawEdge(edge){
-		$("#svg").prepend($("<line x1="+edge.source.center.x+
-				"y1="+edge.source.center.y+
-				" x2="+edge.target.center.x+
-				" y2="+edge.target.center.y+
-				"style=\"stroke:rgb(0,0,0);stroke-width:0.5\" />"););
+		$("#svgDiv").prepend($("<line x1=\""+edge.source.center.x * zoom + graphCenter.x+
+				"\" y1=\""+edge.source.center.y* zoom + graphCenter.y+
+				"\" x2=\""+edge.target.center.x * zoom + graphCenter.x+
+				"\" y2=\""+edge.target.center.y *zoom + graphCenter.y+
+				"\" style=\"stroke:rgb(0,0,0);stroke-width:0.5\" />"));
 		//<line x1="0" y1="0" x2="200" y2="200"
 		//style="stroke:rgb(255,0,0);stroke-width:2"/>
-	}
-
-	function request(id, nbNeighbours){
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		canvas.width = canvas.width;
-
-		$.ajax({
-			  url: '../Server/index.php',
-			  dataType: 'json',
-			  cache: false,
-			  data: {"controller" : "voisinsN", "id" : id, "nn" : nbNeighbours, "action" : "getVoisinsN", "w": canvas.width, "h": canvas.height},
-			  success: function(data) {
-					graph.clearNodes();
-					var liens = data.liens;
-					var positions = data.positions;
-					for(var i in positions){
-						var node = new Node(positions[i][0], positions[i][1], positions[i][2]);
-						graph.addNode(node);
-					}	
-					for(var i in liens){				
-						graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
-						drawEdge(graph.edges[i]);				
-					}
-					for(var i=0; i<graph.nodes.length; i++)
-						drawNode(graph.nodes[i]);
-
-
-			  }
-		});
-			 
-		
-		/*
-		$.getJSON('../Server/index.php?controller=voisinsN&action=getVoisinsN&id='+id+'&nn='+nbNeighbours, function(data) {
-			graph.clearNodes();
-			var liens = data.liens;
-			var positions = data.positions;
-			for(var i in positions){
-				var node = new Node(positions[i][0], positions[i][1] * ratio, positions[i][2] * ratio);
-				graph.addNode(node);
-			}	
-			for(var i in liens){				
-				graph.addEdge(graph.getNode(liens[i][0]), graph.getNode(liens[i][1]));
-				drawEdge(graph.edges[i]);				
-			}
-			for(var i=0; i<graph.nodes.length; i++)
-				drawNode(graph.nodes[i]);
-
-
-		});*/
 	}
